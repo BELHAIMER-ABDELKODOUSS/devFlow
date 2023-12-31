702623bbeaea3222e2c1cdaffae9503ce5c52e21
BELHAIMER-ABDELKODOUSS <abdelkodoussbelhaimerbusiness@gmail.com>
2023-12-31 15:32:19 +0100

    integrate authentication

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+const Home = () => {
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default Home;
